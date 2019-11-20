4dae67515af6af7ca7eed5333fcbad277e73adc2
Otto Cedeno <otto@ottostacos.com>
2019-11-20 02:54:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,41 @@
-# Add  code here!
+require 'benchmark'
+require 'pry'
+
+def prime?(test_number)
+  if test_number > 1
+    number_range = (2..(test_number-1)).to_a
+
+    number_range.each do |number|
+      if test_number % number == 0
+         return false
+      end
+    end
+
+    return true
+
+  end
+
+  return false
+
+end
+
+# Work in progress below....
+# def is_prime?(test_number)
+#   if test_number > 1 && (test_number % 2 == 0) || test_number % 3 == 0 || test_number % 5)
+#     false
+#   else
+#     number_range = (2..test_number-1).to_a
+#
+#     number_range.each do |num|
+#       if test_number % num == 0
+#         false
+#       end
+#   else
+#     false
+# end
+
+
+# Benchmark.bm(27) do |bm|
+#   bm.report('Speed at 20') { prime?(20) }
+#   bm.report('Speed at 2000000') { prime?(2000000) }
+# end

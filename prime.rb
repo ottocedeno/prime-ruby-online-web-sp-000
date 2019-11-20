fa0a10dccf5c749a96dad450c771c9c2f71a62b5
Otto Cedeno <otto@ottostacos.com>
2019-11-20 01:37:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+require 'benchmark'
+require 'pry'
+def prime?(test_number)
+  if test_number > 1
+    number_range = (2..(test_number-1)).to_a
+
+    number_range.each do |number|
+      if test_number % number == 0
+         return false
+      end
+    end
+
+    return true
+
+  end
+
+  return false
+
+end
+
+puts Benchmark.measure {prime?(10000000)}
